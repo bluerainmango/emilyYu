661e64e47ac1ac550cc454ce38acf29e0c8387fd
Emily Yu <bluerainmango@gmail.com>
2019-12-25 00:25:32 -0800

    refactoring:js,css

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $('document').ready(function(){
 /**********************************/
 /*        Materialize init        */
 /**********************************/
+
 	// Sidenav init & hide
 	$('.sidenav').sidenav();
 
@@ -13,10 +14,10 @@ $('document').ready(function(){
 	// carousel, collapsible init
 	$('.carousel').carousel();
 	$('.collapsible').collapsible();
-
+	$('.tooltipped').tooltip();
 
 /**********************************/
-/*  Side Nav - appear/disappear   */
+/*  			Side Nav - on/off   		*/
 /**********************************/
 
 	// Slide in the fixed sidenav according to scrolling
@@ -26,14 +27,16 @@ $('document').ready(function(){
 				
 				if(direction==="down"){
 					
-					$('.sidenav').attr('style','display:block;').addClass('sidenav-fixed')
+					$('.sidenav').attr('style','display:block;').addClass('sidenav-fixed');
 
 				}
 				else{
 
-					if($(window).width() > 992)
-					$('.sidenav').attr('style','display:none;')
+					if( $(window).width() > 992 ){
 
+						$('.sidenav').attr('style','display:none;');
+
+					}
 				}
 
 			},
@@ -43,46 +46,45 @@ $('document').ready(function(){
 	});
 
 /**********************************/
-/*  Side Nav - scroll follower    */
+/*  Side Nav - Scroll Follower    */
+/* :Notifing which menu's content */ 
+/*  is being seen on the screen. 	*/
 /**********************************/
 
-	// Sidenav's Scroll Follower : Notifing which menu's page is being seen on the screen.
 	const scrollFollowers = $('.scrollFollower');
 
 	// SideNav menu class name arr : [about, skill, project...] 
-	const navMenuClassArr = $('.menu').map( (i,el) => 
-														el.getAttribute('class').split(' ')[1] 
-													).get();
+	const navMenuClassArr = $('.menu').map( 
+														function(i,el){ 
+															return el.getAttribute('class').split(' ')[1];
+														}).get();
 
-	// Attach waypoint event to each section.
-	scrollFollowers.each((i,el) => {
+	// Attach a scroll event to each section.
+	scrollFollowers.each(function(i,el){
 
-			new Waypoint({
-					element: el,
-					handler: function(direction){
+			return new Waypoint({
+								element: el,
+								handler: function(direction){
 
-											$('.currentMenu').removeClass('currentMenu');
+														$('.currentMenu').removeClass('currentMenu');
+														let currentIndex;
 
-											let currentIndex;
+														if(direction === "down"){
+															
+															// 992px = Top nav bar appearing point
+															currentIndex = $(window).width() > 992 ? i : i+1;  
+														}
+														else if(direction === "up"){
 
-											if(direction === "down"){
-												
-												// 992px = Top nav bar appearing point
-												currentIndex = $(window).width() > 992 ? i : i+1;  
+															currentIndex = $(window).width() > 992 ? i-1 : i;	
+														}
 
-											}
-											else if(direction === "up"){
-
-												currentIndex = $(window).width() > 992 ? i-1 : i;	
-
-											}
-
-											$(`.${navMenuClassArr[currentIndex]}`).addClass('currentMenu');
-					}
-			});
+														$(`.${navMenuClassArr[currentIndex]}`).addClass('currentMenu');
+								}
+						});
 	});
 	
-	// If menu clicked, close fixed sidenav
+	// If a menu clicked, close the sidenav
 	$('.sidenav li, .sidenav li *').click(function(){
 		$('.sidenav').sidenav('close'); 
 	});
@@ -92,64 +94,82 @@ $('document').ready(function(){
 /*       Header animation   	    */
 /**********************************/
 
-var words = document.getElementsByClassName('word');
-var wordArray = [];
-var currentWord = 0;
+const words = $('.word');
+let wordArr = [];
+let currentWordIndex = 0;
 
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
+$(words[currentWordIndex]).css('opacity',1);
+
+// Splite each words and add it to wordArr
+for (let i = 0; i < words.length; i++) {
 	splitLetters(words[i]);
 }
 
+changeWord();
+setInterval(changeWord, 3000);
+
+
+// Functions for animation
+
 function changeWord() {
-	var cw = wordArray[currentWord];
-	var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
-	for (var i = 0; i < cw.length; i++) {
-		animateLetterOut(cw, i);
+
+	const currentWord = wordArr[currentWordIndex];
+	const nextWord = currentWordIndex === words.length-1 ? wordArr[0] : wordArr[currentWordIndex+1];
+	
+	// Take out current letters
+	for (let i = 0; i < currentWord.length; i++) {
+		animateLetterOut(currentWord, i);
 	}
 	
-	for (var i = 0; i < nw.length; i++) {
-		nw[i].className = 'letter behind';
-		nw[0].parentElement.style.opacity = 1;
-		animateLetterIn(nw, i);
+	// Take in next letters
+	for (let i = 0; i < nextWord.length; i++) {
+
+		$(nextWord[i]).attr('class', 'letter behind');
+		$(nextWord[0]).parent().css('opacity', 1);
+		animateLetterIn(nextWord, i);
 	}
-	
-	currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+
+	// Move to next word
+	currentWordIndex = (currentWordIndex === wordArr.length-1) ? 0 : currentWordIndex+1;
 }
 
-function animateLetterOut(cw, i) {
+function animateLetterOut(currentWord, i) {
+
 	setTimeout(function() {
-		cw[i].className = 'letter out';
+		$(currentWord[i]).attr('class','letter out');
 	}, i*80);
 }
 
-function animateLetterIn(nw, i) {
+function animateLetterIn(nextWord, i) {
+
 	setTimeout(function() {
-		nw[i].className = 'letter in';
+		$(nextWord[i]).attr('class', 'letter in');
 	}, 340+(i*80));
 }
 
 function splitLetters(word) {
-	var content = word.innerHTML;
-	word.innerHTML = '';
-	var letters = [];
-	for (var i = 0; i < content.length; i++) {
-		var letter = document.createElement('span');
-		letter.className = 'letter';
-		letter.innerHTML = content.charAt(i);
-		word.appendChild(letter);
-		letters.push(letter);
+
+	const content = $(word).text();
+
+	$(word).text('');
+	let letters = [];
+
+	for (let i = 0; i < content.length; i++) {
+
+		const $letter = $('<span>');
+		$letter.addClass('letter').text(content.charAt(i));
+		$(word).append($letter);
+		letters.push($letter.get()[0]);
 	}
 	
-	wordArray.push(letters);
+	wordArr.push(letters);
 }
 
-changeWord();
-setInterval(changeWord, 4000);
 
 /**********************************/
 /*        Smooth scrolling        */
 /**********************************/
+
 $("a").on('click', function(event) {
 
 	if (this.hash !== "") {
